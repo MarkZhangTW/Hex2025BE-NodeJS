8316bc1136ff65488e92f3d13dbc2cda75a8e730
Mark Zhang <markzhang1994@gmail.com>
2025-01-11 19:05:57 +0800

    題目三：邏輯運算子

--- week2/index.js
+++ week2/index.js
@@ -51,11 +51,12 @@ let BobPrice = 1800; /* Bob 消費金額 */
 let BobIsVip = false; /* Bob 是否為 VIP */
 
 // 練習：（可自行將下方程式碼的註解刪除，完成答題）
-// if () {
-//   console.Log（"客戶您好，您有符合贈品資格"）；
-// } else {
-//   console.Log（"客戶您好，您沒有符合贈品資格"）
-// }
+if (BobPrice >= giftPriceRule || BobIsVip) {
+  console.log("客戶您好，您有符合贈品資格");
+  giftNum--;
+} else {
+  console.log("客戶您好，您沒有符合贈品資格");
+}
 
 console.log(`贈品還剩下${giftNum}個`);
 
